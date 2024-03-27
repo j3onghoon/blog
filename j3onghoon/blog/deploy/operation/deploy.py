831fa25ed5eb708e79dbff5cc16217e6a5410f8c
jhpark0116 <jhpark0116@mediquitous.com>
2024-03-27 17:15:57 +0900

    제목 수정 및 배포 수정

--- j3onghoon/blog/deploy/operation/deploy.py
+++ j3onghoon/blog/deploy/operation/deploy.py
@@ -1,12 +1,17 @@
-from pyinfra.operations import git, systemd, apt, pip
+from pyinfra.operations import git, systemd, apt, pip, files, server
 
-apt.packages(['python3', 'nginx'], _sudo=True)
 
-git.repo('git@github.com:j3onghoon/blog.git', '/home/ubuntu/blog')
+apt.update(_sudo=True)
+apt.packages(['python3', 'python3-venv', 'nginx'], _sudo=True)
+
+git.repo('https://github.com/j3onghoon/blog.git', '/home/ubuntu/blog')
 
 pip.venv('/home/ubuntu/venv', python='python3')
 pip.packages(requirements='/home/ubuntu/blog/requirements.txt', virtualenv='/home/ubuntu/venv')
 
+server.shell(commands=['/home/ubuntu/venv/bin/python3 /home/ubuntu/blog/j3onghoon/manage.py migrate'])
+
+files.put('j3onghoon/blog/deploy/gunicorn/gunicorn.service', '/etc/systemd/system/gunicorn.service', _sudo=True)
 systemd.daemon_reload(_sudo=True)
 
 systemd.service(service='gunicorn.service', reloaded=True, running=True, enabled=True, _sudo=True)
