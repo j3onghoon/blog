2e387f81f679fb94d118cb0ed07ad1c0edee50f1
jhpark0116 <jhpark0116@mediquitous.com>
2024-03-27 16:53:56 +0900

    배포시 패키지 설치

--- j3onghoon/blog/deploy/operation/deploy.py
+++ j3onghoon/blog/deploy/operation/deploy.py
@@ -1,7 +1,13 @@
-from pyinfra.operations import git, systemd
+from pyinfra.operations import git, systemd, apt, pip
+
+apt.packages(['python3', 'nginx'], _sudo=True)
 
 git.repo('git@github.com:j3onghoon/blog.git', '/home/ubuntu/blog')
+
+pip.venv('/home/ubuntu/venv', python='python3')
+pip.packages(requirments='/home/ubuntu/blog/requirements.txt', virtualenv='/home/ubuntu/venv')
 
 systemd.daemon_reload(_sudo=True)
 
 systemd.service(service='gunicorn.service', reloaded=True, running=True, enabled=True, _sudo=True)
+
