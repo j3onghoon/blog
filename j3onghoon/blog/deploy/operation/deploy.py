3efca36f850970fad83732c453163331944590dc
jhpark0116 <jhpark0116@mediquitous.com>
2024-03-27 16:47:36 +0900

    service 재실행

--- j3onghoon/blog/deploy/operation/deploy.py
+++ j3onghoon/blog/deploy/operation/deploy.py
@@ -1,3 +1,7 @@
-from pyinfra.operations import git
+from pyinfra.operations import git, systemd
 
 git.repo('git@github.com:j3onghoon/blog.git', '/home/ubuntu/blog')
+
+systemd.daemon_reload(_sudo=True)
+
+systemd.service(service='gunicorn.service', reloaded=True, running=True, enabled=True, _sudo=True)
