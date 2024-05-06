5af75ec3856dc1d9183680dd7078e325c54484f1
jhpark0116 <jhpark0116@mediquitous.com>
2024-05-06 20:35:47 +0900

    어드민에 새로운 모델들 추가

--- j3onghoon/blog/admin.py
+++ j3onghoon/blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Work, WorkDetail, WorkKeyWord, Career
 
 admin.site.register(Post)
+admin.site.register(Career)
+admin.site.register(Work)
+admin.site.register(WorkDetail)
+admin.site.register(WorkKeyWord)
