13c056e14515ae0113d355d627238f00dcff3df1
jhpark0116 <jhpark0116@mediquitous.com>
2024-05-06 20:30:04 +0900

    이력서 수정 및 이력서를 위한 모델 추가

--- j3onghoon/blog/models.py
+++ j3onghoon/blog/models.py
@@ -16,3 +16,26 @@ class Post(models.Model):
 
 class Category(models.Model):
     name = CharField(max_length=100)
+
+
+class Career(models.Model):
+    position = CharField(max_length=100)
+    company_name = CharField(max_length=100)
+    company_link = CharField(max_length=100)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(null=True)
+
+
+class Work(models.Model):
+    career = models.ForeignKey('Career', on_delete=models.CASCADE)
+    overview = models.CharField(max_length=100)
+
+
+class WorkDetail(models.Model):
+    work = models.ForeignKey('Work', on_delete=models.CASCADE)
+    detail = models.TextField()
+
+
+class WorkKeyWord(models.Model):
+    work = models.ForeignKey('Work', on_delete=models.CASCADE)
+    keyword = models.CharField(max_length=100)
